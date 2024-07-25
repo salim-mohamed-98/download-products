c2f2fdeed1b5c8c81ca88e904ec443b6c92c5e68
salim-mohamed-98 <salim_mohamed98@hotmail.com>
2024-07-25 04:56:58 +0200

    add dark/light mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/shadcn/ui/button";
+import { ModeToggle } from "@/components/shadcn/ui/mode-toggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="p-3">
-      <Button>Click me!</Button>
+      <Button className="dark:bg-red-400">Click me!</Button>
+      <ModeToggle />
     </main>
   );
 }
