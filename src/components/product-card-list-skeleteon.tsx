fc90f44e7cb5a5f62e8450d1476739f35bfadd83
salim-mohamed-98 <salim_mohamed98@hotmail.com>
2024-08-03 14:41:50 +0200

    add missing element key prop in iterator

--- src/components/product-card-list-skeleteon.tsx
+++ src/components/product-card-list-skeleteon.tsx
@@ -12,7 +12,7 @@ export default function ProductCardListSkeleton({ number_products }: Props) {
         <div className="max-w-7xl mx-auto py-4 px-4 overflow-hidden sm:py-8 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 lg:gap-x-8">
             {Array.from({ length: number_products }).map((_, index) => (
-              <article className="group text-sm space-y-4">
+              <article key={index} className="group text-sm space-y-4">
                 <Skeleton className="w-full aspect-square" />
                 <div className="space-y-2">
                   <Skeleton className="h-6 w-[250px]" />
